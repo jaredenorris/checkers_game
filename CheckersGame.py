a3b1c6fa21b79c0b4ed7dc349ad488e5909dcb32
jaredenorris <jarednorris72@gmail.com>
2023-03-10 20:08:18 -0800

    1st

--- CheckersGame.py
+++ CheckersGame.py
@@ -94,13 +94,27 @@ class Checkers:
                         raise InvalidSquare
 
         # move piece
-        destination_row = self._board[destination_square_location[0]]
-        for row in self._board:
-            if row == destination_row:
-                row[destination_square_location[1]] = start_pos
-        for start in self._board:
-            if start == start_row:
-                start[starting_square_location[1]] = None
+        self._board[destination_square_location[0]][destination_square_location[1]] = start_pos
+        self._board[starting_square_location[0]][starting_square_location[1]] = None
+
+        # if capture move check if piece is king
+        if abs(starting_square_location[0] - destination_square_location[0]) > 1:
+            if start_pos == "BK ":
+                self.king_capture(player_name, starting_square_location, destination_square_location)
+                captured_pieces += 1
+                return captured_pieces
+            if start_pos == "WK ":
+                self.king_capture(player_name, starting_square_location, destination_square_location)
+                captured_pieces += 1
+                return captured_pieces
+            if start_pos == "BTK ":
+                self.king_capture(player_name, starting_square_location, destination_square_location)
+                captured_pieces += 1
+                return captured_pieces
+            if start_pos == "WTK ":
+                self.king_capture(player_name, starting_square_location, destination_square_location)
+                captured_pieces += 1
+                return captured_pieces
 
         # capture piece
         if abs(starting_square_location[0] - destination_square_location[0]) > 1:
@@ -130,15 +144,89 @@ class Checkers:
                         player.set_captured_pieces_count(1)
                         captured_pieces += 1
 
-
-
-
+        #promote to king
+        if self._turn % 2 == 0: #Black made it to other side
+            if destination_square_location[0] == 0:
+                self._board[destination_square_location[0]][destination_square_location[1]] = "BK "
+                for player in self._players:
+                    if player.get_piece_color() == "Black":
+                        player.set_king_count(1)
+        if self._turn % 2 != 0: #White made it to other side
+            if destination_square_location[0] == 7:
+                self._board[destination_square_location[0]][destination_square_location[1]] = "WK "
+                for player in self._players:
+                    if player.get_piece_color() == "White":
+                        player.set_king_count(1)
 
         self._turn += 1
         return captured_pieces
 
+    def king_capture(self, player_name, starting_square_location, destination_square_location):
+        """method defines rules for a king capturing opponents"""
+        start_row = starting_square_location[0]
+        start_column = starting_square_location[1]
+        destination_row = destination_square_location[0]
+        destination_column = destination_square_location[1]
+        distance = abs(start_row - destination_row)
 
 
+        # king jumps up
+        if destination_row < start_row:
+            if destination_column > start_column: # king jumps right
+                count = 1
+                while count < distance:
+                    destination_row += 1
+                    destination_column -= 1
+                    self._board[destination_row][destination_column] = None
+                    count += 1
+            if destination_column < start_column: # king jumps left
+                count = 1
+                while count < distance:
+                    destination_row += 1
+                    destination_column += 1
+                    self._board[destination_row][destination_column] = None
+                    count += 1
+        # king jumps down
+        if destination_row > start_row:
+            if destination_column > start_column: # king jumps right
+                count = 1
+                while count < distance:
+                    destination_row -= 1
+                    destination_column -= 1
+                    self._board[destination_row][destination_column] = None
+                    count += 1
+            if destination_column < start_column: # king jumps left
+                count = 1
+                while count < distance:
+                    destination_row -= 1
+                    destination_column += 1
+                    self._board[destination_row][destination_column] = None
+                    count += 1
+        #promote triple king
+        destination_row = destination_square_location[0]
+        destination_column = destination_square_location[1]
+        destination_pos = self._board[destination_row][destination_column]
+        if destination_pos == "WK ":
+            if destination_row == 0:
+                self._board[destination_row][destination_column] = "WTK"
+                for player in self._players:
+                    if player.get_player_name() == player_name:
+                        player.set_triple_king_count(1)
+                        player.set_king_count(-1)
+        if destination_pos == "BK ":
+            if destination_row == 7:
+                self._board[destination_row][destination_column] = "BTK"
+                for player in self._players:
+                    if player.get_player_name() == player_name:
+                        player.set_triple_king_count(1)
+                        player.set_king_count(-1)
+
+
+
+        self._turn += 1
+        for player in self._players:
+            if player.get_player_name() == player_name:
+                player.set_captured_pieces_count(1)
 
 
 
@@ -244,6 +332,14 @@ class Player:
         """sets captured pieces count"""
         self._captured_pieces_count += piece
 
+    def set_king_count(self, king):
+        """sets number of kings player has"""
+        self._king_count += king
+
+    def set_triple_king_count(self, king):
+        """sets number of triple kings player has"""
+        self._triple_king_count += king
+
 def main():
     """Exception handling for exception classes"""
     test = Checkers()
@@ -278,7 +374,51 @@ if __name__ == '__main__':
 #game.print_board()
 #game.play_game("Joe", (4, 1), (2, 3))
 #game.print_board()
-#print(game.game_winner())
-
+#game.play_game("Jim", (1, 2), (2, 1))
+#game.print_board()
+#game.play_game("Joe", (5, 6), (4, 5))
+#game.print_board()
+#game.play_game("Jim", (0, 1), (1, 2))
+#game.print_board()
+#game.play_game("Joe", (2, 3), (0, 1))
+#game.print_board()
+#game.play_game("Jim", (3, 0), (4, 1))
+#game.print_board()
+#game.play_game("Joe", (6, 5), (5, 6))
+#game.print_board()
+#game.play_game("Jim", (4, 1), (5, 2))
+#game.print_board()
+#game.play_game("Joe", (7, 4), (6, 5))
+#game.print_board()
+#game.play_game("Jim", (5, 2), (7, 4))
+#game.print_board()
+#game.play_game("Joe", (5, 6), (4, 7))
+#game.print_board()
+#game.play_game("Jim", (7, 4), (5, 6))
+#game.print_board()
+#game.play_game("Joe", (4, 5), (3, 4))
+#game.print_board()
+#game.play_game("Jim", (5, 6), (2, 3))
+#game.print_board()
+#game.play_game("Joe", (7, 2), (6, 3))
+#game.print_board()
+#game.play_game("Jim", (2, 3), (4, 1))
+#game.print_board()
+#game.play_game("Joe", (5, 4), (4, 5))
+#game.print_board()
+#game.play_game("Jim", (1, 4), (2, 3))
+#game.print_board()
+#game.play_game("Joe", (0, 1), (3, 4))
+#game.print_board()
+#game.play_game("Jim", (4, 1), (2, 3))
+#game.print_board()
+#game.play_game("Joe", (3, 4), (5, 2))
+#game.print_board()
+#game.play_game("Jim", (2, 3), (0, 1))
+#game.print_board()
+#ame.play_game("Joe", (5, 2), (7, 4))
+#game.print_board()
+#game.play_game("Jim", (0, 1), (5, 6))
+#game.print_board()
 
 
---
+++ CheckersGameTester.py
@@ -0,0 +1,10 @@
+#Author: Jared Norris
+#Github username: jaredenorris
+#Date: 3/10/23
+#Description: Unit tests for CheckersGame.py
+
+import unittest
+from CheckersGame import Checkers, Player
+
+class TestCheckers(unittest.TestCase):
+    """Test Checkers class methods"""
